b9d0de25fd45e29fd696c031a42b92313034df4f
jesse <hu.li.fa@foocaa.com>
2017-03-06 17:16:44 +0800

    first commit

---
+++ tests/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Decription:
+ * Author: Jesse Hu (hu2014jesse@gmail.com)
+ * Date: 2017/03/06 下午 2:44
+ * Project: fookii-validate
+ */
+
+namespace Fookii\ValidateTest;
+
+require "../vendor/autoload.php";
+
+$testor = new validateTest();
+
+/*test-01*/
+$testor->testValidateData();
